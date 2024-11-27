b4d457907eadf0fe5f9d5ed70521ba9f4bab5b91
Ivan Timonin <Tyuiu.TimoninIA@outlook.com>
2024-11-27 19:52:16 +0500

    Task3

--- Tyuiu.TimoninIA.Sprint5.Task3.V29.Lib/DataService.cs
+++ Tyuiu.TimoninIA.Sprint5.Task3.V29.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.TimoninIA.Sprint5.Task3.V29.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask3.bin";
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask3.bin" });
             double z = Math.Pow(x, 3) + 2 * Math.Pow(x, 2) + 5 * x + 4;
             z = Math.Round(z, 3);
             using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
